0e3abe20f6decafcaec0c904652c8a184149926a
anullihate <lclarkdwain@outlook.com>
2019-09-05 23:47:26 +0800

    no void

--- src/main/java/dev/anullihate/envyskyblockcore/listeners/PlayerEventListener.java
+++ src/main/java/dev/anullihate/envyskyblockcore/listeners/PlayerEventListener.java
@@ -4,6 +4,9 @@ import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerMoveEvent;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
 import dev.anullihate.envyskyblockcore.Core;
 
 public class PlayerEventListener implements Listener {
@@ -18,5 +21,18 @@ public class PlayerEventListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         player.setMovementSpeed(0.1f);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Level level = this.core.getServer().getDefaultLevel();
+        int x = this.core.getServer().getDefaultLevel().getSafeSpawn().getFloorX();
+        int y = this.core.getServer().getDefaultLevel().getSafeSpawn().getFloorY();
+        int z = this.core.getServer().getDefaultLevel().getSafeSpawn().getFloorZ();
+
+        if (event.getTo().getFloorY() < 0) {
+            event.getPlayer().teleport(new Position(x, y, z, level));
+        }
     }
 }
