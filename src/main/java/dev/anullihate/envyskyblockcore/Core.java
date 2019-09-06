a1232a365c0a77f1d14f9eae9e48c2536a13b83d
anullihate <lclarkdwain@outlook.com>
2019-09-06 19:28:41 +0800

    1

--- src/main/java/dev/anullihate/envyskyblockcore/Core.java
+++ src/main/java/dev/anullihate/envyskyblockcore/Core.java
@@ -1,5 +1,6 @@
 package dev.anullihate.envyskyblockcore;
 
+import cn.nukkit.level.Level;
 import cn.nukkit.plugin.PluginBase;
 import dev.anullihate.envyskyblockcore.listeners.PlayerEventListener;
 
@@ -8,5 +9,23 @@ public class Core extends PluginBase {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new PlayerEventListener(this), this);
+
+
+        this.initialize();
+    }
+
+    public void initialize() {
+        for (Level level : this.getServer().getLevels().values()) {
+            if (level.getName().equals("skyblockhub")) {
+                level.checkTime();
+                level.setTime(Level.TIME_MIDNIGHT);
+                level.stopTime();
+                level.checkTime();
+            } else {
+                level.checkTime();
+                level.startTime();
+                level.checkTime();
+            }
+        }
     }
 }
